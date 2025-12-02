e9a24122c6c1121dd7169f8a3a113b430ec5f6ce
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-12-02 19:21:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 //your JS code here. If required.
-const inputs = document.querySelectorAll(".otp-box input");
+const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
+
+    // Forward typing
     input.addEventListener("input", () => {
-        input.value = input.value.replace(/[^0-9]/g, ""); // allow only numbers
-        if (input.value !== "" && index < inputs.length - 1) {
+        input.value = input.value.replace(/[^0-9]/g, "");
+
+        if (input.value && index < inputs.length - 1) {
             inputs[index + 1].focus();
         }
     });
 
+    // Backspace handling
     input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace" && input.value === "" && index > 0) {
-            inputs[index - 1].focus();
+        if (e.key === "Backspace") {
+            if (input.value === "") {
+                if (index > 0) {
+                    inputs[index - 1].value = "";
+                    inputs[index - 1].focus();
+                }
+            } else {
+                input.value = "";
+            }
         }
     });
 });
-
-
-   
